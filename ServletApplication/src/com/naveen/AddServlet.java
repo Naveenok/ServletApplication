29c3cb2d2d14bdf881c7b97b42d40273a392b747
Naveen.O.K <oknaveen9395@gmail.com>
2018-06-04 08:29:14 +0530

    web.xml configuration done and the servlet class added

--- ServletApplication/src/com/naveen/AddServlet.java
+++ ServletApplication/src/com/naveen/AddServlet.java
@@ -3,12 +3,24 @@
  */
 package com.naveen;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
- * @author admin
+ * @author Naveen Kumaravelmurugaian
  *
  */
-public class AddServlet extends HttpServlet{
-
+public class AddServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i + j;
+		PrintWriter resp = res.getWriter();
+		// System.out.println(k);
+		resp.println("The result is " + k);
+	}
 }
