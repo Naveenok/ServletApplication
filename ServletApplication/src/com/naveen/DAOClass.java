1e1d438a5173b106978f4cec7898d7346ab54cb7
Naveen.O.K <oknaveen9395@gmail.com>
2018-06-12 21:48:41 +0530

    Jsp page,DAO class are created and the result displayed in jsp page

---
+++ ServletApplication/src/com/naveen/DAOClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.naveen;
+
+/**
+ * @author Naveen Kumaravelmurugaian
+ *
+ */
+public class DAOClass {
+	public NumberClass add(Integer n1, Integer n2) {
+		NumberClass obj1 = new NumberClass();
+		obj1.setNum1(n1);
+		obj1.setNum2(n2);
+	
+		Integer answer = n1 + n2;
+		obj1.setAns(answer);
+		System.out.println(obj1);
+		return obj1;
+
+	}
+}
---
+++ ServletApplication/src/com/naveen/MyServlet.java
@@ -0,0 +1,37 @@
+package com.naveen;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+// @WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
+		Integer n1 = Integer.parseInt(req.getParameter("snum1"));
+		Integer n2 = Integer.parseInt(req.getParameter("snum2"));
+		// Integer answ = n1 + n2;
+		DAOClass Dobj = new DAOClass();
+		NumberClass nObj = Dobj.add(n1, n2);
+		Integer answ = nObj.getAns();
+
+		/*
+		 * s PrintWriter p1 = res.getWriter();
+		 * p1.println(Integer.toString(answer));
+		 */
+		// req.setAttribute("a", Integer.toString(answer));
+		req.setAttribute("answer", answ);
+		RequestDispatcher s1 = req.getRequestDispatcher("displaypage.jsp");
+		s1.forward(req, res);
+	}
+
+}
